e7fabf647f038d6ab41495f644a88a842b25eb47
Jean <62147746+JeanS-github@users.noreply.github.com>
2021-08-23 21:11:58 +0200

    add function and more details

--- calculate-hours.py
+++ calculate-hours.py
@@ -6,7 +6,8 @@ from datetime import timedelta
 import pandas as pd
 
 # read csv file
-df = pd.read_csv("check-in.csv", skiprows=[0],sep=';')
+my_file="check-in.csv"
+df = pd.read_csv(my_file, skiprows=[0],sep=';')
 
 #df.info()
 
@@ -18,49 +19,68 @@ date_semaine_prev = datetime.strptime(semaine_prev, '%d/%m/%Y %H:%M')
 # week number
 id_semaine_prev = date_semaine_prev.strftime("%W")
 # number of hours I need to do for 35h/week
-left1 = left = timedelta(hours=0,minutes=0)
+left1 = left = plus1 = plus = timedelta(hours=0,minutes=0)
 
+# display time in sepcific format
+def format_time(self):
+    heures=int(self.total_seconds()/3600)
+    minutes=((self.total_seconds()/3600)-heures)*60
+    my_format=f"{heures}h {round(minutes)}m"
+    return my_format
+
+nb_weeks=0
 Total = timedelta(hours=0,minutes =0)
 for index in df.index:
     time1 = df.loc[index,'Début']
     time2 = df.loc[index,'Fin']
     d1 = datetime.strptime(time1, '%d/%m/%Y %H:%M')
     id_semaine = d1.strftime("%W")
-    #print("id semaine =",id_semaine)
     d2 = datetime.strptime(time2, '%d/%m/%Y %H:%M')
+    # difference between the 2 times clocking
     d = d2-d1
     if id_semaine == id_semaine_prev:
         cumul_semaine += d
     else:
-        heures=int(cumul_semaine.total_seconds()/3600)
-        minutes=((cumul_semaine.total_seconds()/3600)-heures)*60
-        print("semaine",id_semaine_prev,"=>",heures,"h",round(minutes),"m")
+        nb_weeks+=1
+        print(f"semaine {id_semaine_prev} => {format_time(cumul_semaine)}")
         if cumul_semaine < timedelta(hours=35):
             left=timedelta(hours=35)-cumul_semaine
             print("il manque :",left,"")
             left1 += left
+        else: # heures supp
+            plus=cumul_semaine-timedelta(hours=35)
+            plus1+=plus
         print()
         cumul_semaine = d
         id_semaine_prev = id_semaine
     # display the time at each time clocking
     Total = Total + d
-    print("nb heure",d)
+    print(d1.strftime("%d/%m"),"nb heure",d)
+
 # display total hours for the last week
-heures=int(cumul_semaine.total_seconds()/3600)
-minutes=((cumul_semaine.total_seconds()/3600)-heures)*60
-print("semaine",id_semaine_prev,"=>",heures,"h",round(minutes),"m")
+print(f"semaine {id_semaine_prev} => {format_time(cumul_semaine)}")
 if cumul_semaine < timedelta(hours=35):
     left=timedelta(hours=35)-cumul_semaine
     print("il manque :",left,"")
     left1 += left
+else: # heures supp
+    plus=cumul_semaine-timedelta(hours=35)
+    plus1+=plus
 
 # Global total
-heures=int(Total.total_seconds()/3600)
-minutes=((Total.total_seconds()/3600)-heures)*60
-print("\nDurée totale    :",Total)
-#print("Durée en heures2: ",Total.total_seconds()/60/60)
-print("Durée en heures :",heures,"h",round(minutes),"m")
-heures=int(left1.total_seconds()/3600)
-minutes=((left1.total_seconds()/3600)-heures)*60
-print("Il me manque    :",heures,"h",round(minutes),"m")
+print("Durée totale    :",Total)
+print("Durée en heures :",format_time(Total))
+print("Nombre de semaines de travail :",nb_weeks+1)
 
+# Total heures qu'il me manque
+# Calcul avec les heures supplementaires et manquantes
+if left1 > plus1:
+    left2=left1-plus1
+    format_time(left2)
+    texte=f"Il me manque {format_time(left2)}."
+else:
+    plus2=plus1-left1
+    texte=f"J'ai fait {format_time(plus2)} supp"
+
+# Vrai total heures manquantes ou supp
+print(f'{texte}')
